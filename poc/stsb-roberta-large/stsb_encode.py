fd1c02636938ce58cb819933018085f2d1576c22
Aishwarya Anegundi <24752676+aishwaryaanegundi@users.noreply.github.com>
2021-06-25 17:57:05 +0200

    creates index with only relevant tweets based on the annotations of clusters to remove general tweets

--- poc/stsb-roberta-large/stsb_encode.py
+++ poc/stsb-roberta-large/stsb_encode.py
@@ -29,8 +29,8 @@ def get_combined_dataset(paths):
     data = pd.concat((pd.read_csv(file) for file in tqdm(paths)))
     return data
 
-data = get_combined_dataset(dataset_paths)
-print("Number of tweets in the dataset: ", data.shape[0])
+# data = get_combined_dataset(dataset_paths)
+# print("Number of tweets in the dataset: ", data.shape[0])
 
 # takes list of tweets as input and returns list of pre-processed tweets as output
 def preprocess(tweets):
@@ -45,27 +45,28 @@ def preprocess(tweets):
         processed_tweets.append(result)
     return processed_tweets
 
-# Filtering data between 01.01.2019 to 01.07.2017
-data['tweet_time'] = pd.to_datetime(data['tweet_time'], format = '%Y-%m-%d')
-start_date = '2016-01-01'
-end_date = '2017-07-01'
-mask = (data['tweet_time'] > start_date) & (data['tweet_time'] <= end_date)
-data = data.loc[mask]
-print("Number of tweets in the dataset after filtering: ", data.shape[0])
+# Filtering data between 01.01.2016 to 01.07.2017
+# data['tweet_time'] = pd.to_datetime(data['tweet_time'], format = '%Y-%m-%d')
+# start_date = '2016-01-01'
+# end_date = '2017-07-01'
+# mask = (data['tweet_time'] > start_date) & (data['tweet_time'] <= end_date)
+# data = data.loc[mask]
+# print("Number of tweets in the dataset after filtering: ", data.shape[0])
 
 
 # extracts just the english tweets by using the language tag
-is_english_tweet = data['tweet_language'] == 'en'
-f_english_data = data[is_english_tweet]
-print("Number of English tweets in the filtered dataset: ", f_english_data.shape[0])
+# is_english_tweet = data['tweet_language'] == 'en'
+# f_english_data = data[is_english_tweet]
+# print("Number of English tweets in the filtered dataset: ", f_english_data.shape[0])
 
 # removes the retweets
-is_retweet = f_english_data['retweet_tweetid'].notnull()
-f_english_data = f_english_data[is_retweet]
-print("Number of entries in the dataset after removing retweets: ", f_english_data.shape[0])
+# is_retweet = f_english_data['retweet_tweetid'].notnull()
+# f_english_data = f_english_data[~is_retweet]
+# print("Number of entries in the dataset after removing retweets: ", f_english_data.shape[0])
 
-f_english_tweet_data = f_english_data[['tweetid', 'tweet_text']]
-
+# f_english_tweet_data = f_english_data[['tweetid', 'tweet_text']]
+data = pd.read_csv('./relevant_tweets.csv')
+f_english_tweet_data = data[['tweetid', 'tweet_text']]
 tweets = f_english_tweet_data['tweet_text']
 tweets = preprocess(tweets)
 
@@ -121,13 +122,13 @@ if __name__ == '__main__':
     #Compute the embeddings using the multi-process pool
     start_time = time.perf_counter()
     print('Beginning to encode\n')
-#     encodings = model.encode_multi_process(sentences, pool)
+    encodings = model.encode_multi_process(sentences, pool)
     end_time = time.perf_counter()
     print("Time taken for encoding is: ", (end_time - start_time)/60.0 ," minutes")
     print("Number of dimensions in the encodings", encodings.shape)
     # save the encodings for later use. Order preserved
-#     np.save('encodings_stsb_roberta_large_deduplicated', encodings)
-    encodings = np.load('encodings_stsb_roberta_large_deduplicated.npy')
+    np.save('encodings_stsb_roberta_relevant_tweets', encodings)
+    encodings = np.load('encodings_stsb_roberta_relevant_tweets.npy')
     print('shape of the encoding after reloading: ', encodings.shape)
 #     model.stop_multi_process_pool(pool)
   
@@ -176,23 +177,24 @@ if __name__ == '__main__':
     print('starting from iteration: ', iterated)
     iteration = 0
     for chunk in pd.read_json(filename,lines = True, chunksize=10000):
-        output_filename = '/INET/state-trolls/work/state-trolls/reddit_dataset/comments/scores/' + inputfile + '/' + str(iteration)+'_scores_stsb.txt'
-        with open(output_filename, 'w') as outfile:
-            print('Length of chunk before removing automoderator posts: ', len(chunk))
-            chunk = chunk[chunk.author != 'AutoModerator']
-            print('Length of chunk after removing automoderator posts: ', len(chunk))
-            id_sentences = []
-            if ((iteration >= iterated) & (iteration < iterated + 870)):
+        output_filename = '/INET/state-trolls/work/state-trolls/reddit_dataset/comments/scores/' + inputfile +'/' +str(iteration)+'_scores_stsb.txt'
+#         with open(output_filename, 'w') as outfile:
+        print('Length of chunk before removing automoderator posts: ', len(chunk))
+        chunk = chunk[chunk.author != 'AutoModerator']
+        print('Length of chunk after removing automoderator posts: ', len(chunk))
+        id_sentences = []
+        if ((iteration >= iterated) & (iteration < iterated + 1100)):
+            with open(output_filename, 'w') as outfile:
                 start_time = time.perf_counter()
-                sent_tokenize_begin = time.perf_counter()
+#                 sent_tokenize_begin = time.perf_counter()
                 for index, row in chunk.iterrows():
                     sentences = nltk.sent_tokenize(row['body'])                
                     for idx,sentence in enumerate(sentences):
                         id_sentences.append({'id':row['id'], 'body': sentence, 's_id': idx})
-                sent_tokenize_end = time.perf_counter()
-                print("Time taken for sentence tokenization is :",
-                      (sent_tokenize_end - sent_tokenize_begin)/60.0 ," minutes")
-                print('length of sentences is: ', len(id_sentences))
+#                 sent_tokenize_end = time.perf_counter()
+#                 print("Time taken for sentence tokenization is :",
+#                       (sent_tokenize_end - sent_tokenize_begin)/60.0 ," minutes")
+#                 print('length of sentences is: ', len(id_sentences))
                 sentence_df = pd.DataFrame(id_sentences)
                 sentence_texts = sentence_df['body']
                 sentence_texts = preprocess(sentence_texts)
@@ -208,29 +210,29 @@ if __name__ == '__main__':
                 is_not_empty_string = chunk_id_body['preprocessed_body'].apply(lambda x: not x == '')
                 chunk_id_body = chunk_id_body[is_not_empty_string]
                 # encode the comments
-                start_time_e = time.perf_counter()
+#                 start_time_e = time.perf_counter()
                 encoded_comments = model.encode_multi_process(chunk_id_body['preprocessed_body'].to_list(),
                                                               pool,batch_size = 512)
-                end_time_e = time.perf_counter()
-                print("Time taken for encoding comments is :", (end_time_e - start_time_e)/60.0 ,
-                      " minutes. Iteration: ", iteration)
+#                 end_time_e = time.perf_counter()
+#                 print("Time taken for encoding comments is :", (end_time_e - start_time_e)/60.0 ,
+#                       " minutes. Iteration: ", iteration)
                 query = normalize_rows(encoded_comments)
-                start_time_s = time.perf_counter()
+#                 start_time_s = time.perf_counter()
                 D, I = gpu_index.search(query, 1000) 
-                end_time_s = time.perf_counter()
-                print("Time taken for index search: ", (end_time_s - start_time_s)/60.0 ," minutes")
+#                 end_time_s = time.perf_counter()
+#                 print("Time taken for index search: ", (end_time_s - start_time_s)/60.0 ," minutes")
     #             repeat_search_list = []
 
-                start_time_r = time.perf_counter()
+#                 start_time_r = time.perf_counter()
                 repeat_search_list = ((np.where(np.all(D > 0.65, axis = 1)))[0]).tolist()
     #             for loc, entry in enumerate(chunk_id_body.iterrows()):
     #                 scores = D[loc]
     #                 if (scores[999] > 0.65):
     #                     repeat_search_list.append(loc)
-                end_time_r = time.perf_counter()
-                print("Time taken for repeated search list: ",(end_time_r - start_time_r)/60.0 ," minutes")
-                print('length of repeated search list: ', len(repeat_search_list))
-                start_time_l = time.perf_counter()
+#                 end_time_r = time.perf_counter()
+#                 print("Time taken for repeated search list: ",(end_time_r - start_time_r)/60.0 ," minutes")
+#                 print('length of repeated search list: ', len(repeat_search_list))
+#                 start_time_l = time.perf_counter()
                 post_ids = chunk_id_body['id'].tolist()
                 sent_ids = chunk_id_body['s_id'].tolist()
                 for loc, post_id in enumerate(post_ids):
@@ -251,24 +253,25 @@ if __name__ == '__main__':
                             result_df['sent_id'] = s_ids
 #                             with open(output_filename, 'a') as outfile:
                             r = result_df.to_json(orient = "records")
+#                             print(result_df.shape) 
                             outfile.write(r)
                 end_time = time.perf_counter()
-                print("Time taken for looping: ",
-                      iteration, 'is : ', (end_time - start_time_l)/60.0 ," minutes")
-                print("Time taken for iteration before repeated search is: ",
-                      iteration, 'is : ', (end_time - start_time)/60.0 ," minutes")
+#                 print("Time taken for looping: ",
+#                       iteration, 'is : ', (end_time - start_time_l)/60.0 ," minutes")
+#                 print("Time taken for iteration before repeated search is: ",
+#                       iteration, 'is : ', (end_time - start_time)/60.0 ," minutes")
 
                 k = 20000
                 while (len(repeat_search_list) > 0):
                     chunk_id_body = chunk_id_body.iloc[repeat_search_list]
                     print('length of chunk for only entries to be researched: ', len(chunk_id_body))
                     # encode the comments
-                    start_time_e = time.perf_counter()
+#                     start_time_e = time.perf_counter()
                     encoded_comments = model.encode_multi_process(chunk_id_body['preprocessed_body'].to_list(),
                                                                   pool,batch_size = 512)
-                    end_time_e = time.perf_counter()
-                    print("Time taken for encoding comments (repeateds search) is :", (end_time_e - start_time_e)/60.0 ,
-                          " minutes. Iteration: ", iteration)
+#                     end_time_e = time.perf_counter()
+#                     print("Time taken for encoding comments (repeateds search) is :", (end_time_e - start_time_e)/60.0 ,
+#                           " minutes. Iteration: ", iteration)
                     query = normalize_rows(encoded_comments)
 
                     D, I = index_cpu.search(query, k) 
@@ -304,14 +307,14 @@ if __name__ == '__main__':
                     k = k + 10000
                 end_time = time.perf_counter()
                 print("Time taken for iteration ", iteration, 'is : ', (end_time - start_time)/60.0 ," minutes")
-            elif not (iteration < iterated + 870):
-                break
-            iteration = iteration + 1
+        elif not (iteration < iterated + 1100):
+            break
+        iteration = iteration + 1
     print('iteration to be written to file: ', iteration)
     with open("json_read_iteration.txt", "w") as file1: 
          file1.write(str(iteration))
 
-    if (iteration < iterated + 870):
+    if (iteration < iterated + 1100):
         with open("job_status.txt", "w") as file1: 
             file1.write(str('1'))
 
